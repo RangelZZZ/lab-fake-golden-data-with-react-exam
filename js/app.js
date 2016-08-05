65b555bc3417e911d023ff87810df9ae34ff8b03
RangelZZZ <18792561236@163.com>
2016-08-05 08:48:17 +0800

    delete elements Please enter the commit message for your changes. Lines starting

--- js/app.js
+++ js/app.js
@@ -15,12 +15,17 @@ const App = React.createClass({
         elements.push(element)
         this.setState(elements)
     },
+    deletElement: function (index) {
+        const elements = this.state.elements;
+        elements.splice(index)
+        this.setState(elements)
+    },
     render: function () {
         const isEditor = this.state.isEditor
         return <div>
             <button onClick={this.toggle}>{isEditor ? "preview" : "edit"}</button>
             <div className={isEditor ? "" : "hidden"}>
-                <Editor elements={this.state.elements} onAdd={this.addElement}/>
+                <Editor elements={this.state.elements} onAdd={this.addElement} ondelete={this.deletElement}/>
             </div>
             <div className={isEditor ? "hidden" : ""}>
                 <Preview/>
@@ -32,18 +37,21 @@ const App = React.createClass({
 const Editor = React.createClass({
     render: function () {
         return <div>
-            <Left elements={this.props.elements}/>
+            <Left elements={this.props.elements} ondelete={this.props.ondelete}/>
             <Right elements={this.props.onAdd}/>
         </div>
     }
 })
 
 const Left = React.createClass({
+    delete:function (index) {
+      this.props.ondelete(index)
+    },
     render: function () {
         const elements = this.props.elements.map((ele, index)=> {
             return <div key={index}>
                 <input type={ele}/>
-                <button>-</button>
+                <button onClick={this.delete.bind(this,index)}>-</button>
             </div>
         });
 
